ae72e624c2ddbfc6cfd8a6c7b3fbe4a43d2eb628
Stirling Luke <s.luke@EDU.epsb.ca>
2022-10-07 11:11:32 -0600

    AAAAAAAA

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 // JavaScript Code
+// Button Event Listener
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked() {
+  // Input
+  let name = document.getElementById("char-in").value;
+}
