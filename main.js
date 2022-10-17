aac531162a31e4caec88ef7a677a8e5b056fc1f4
Stirling Luke <s.luke@EDU.epsb.ca>
2022-10-17 10:40:41 -0600

    AAAAAAAAAAAAAAA

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 // JavaScript Code
 // Button Event Listener
+let acronymEl = document.getElementById("acronym");
+
 document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
   // Input
-  let name = document.getElementById("char-in").value;
+  let name = document.getElementById("acro-in").value;
+
+  if (name === "RSVP") {
+  document.getElementById("acro-in").innerHTML = "RSVP";
+  }
+
+  else {
+
+  }
 }
 
